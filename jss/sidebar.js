6ffaac1509690f8d9a4adef533b74b0c5d57ae7c
Md. Fakhrul Alam Siddiqei Antor <50808070+FakhrulASA@users.noreply.github.com>
2020-03-04 13:46:19 +0600

    Add files via upload

--- jss/sidebar.js
+++ jss/sidebar.js
@@ -10,3 +10,11 @@ function closeNav() {
   document.getElementById("mybody").style.marginLeft="0";
   document.getElementById("menu").onclick = openNav;
 }
+function scrollWin() {
+  window.location = 'file:///C:/Personal%20WEBSITE/index.html#jumpHere';
+ 
+}
+function scrollWin2() {
+  window.location = 'file:///C:/Personal%20WEBSITE/index.html#jumpHereZ';
+ 
+}
